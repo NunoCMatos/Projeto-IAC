6468c66d4ec09dc8bd252d64442c6da87b8273c5
Nuno Matos <nunocmatos@tecnico.ulisboa.pt>
2023-05-20 12:53:16 +0100

    Projeto

--- projeto-beyond-Mars.asm
+++ projeto-beyond-Mars.asm
@@ -51,14 +51,21 @@ SPAWN1_COL  EQU 0       ; coluna do 1.º spawnpoint (canto superior esquerdo)
 SPAWN2_COL  EQU 30      ; coluna do 2.º spawnpoint (centro superior)
 SPAWN3_COL  EQU 58      ; coluna do 3.º spawnpoint (canto superior direito)
 
-SPAWN
+SPAWN_SND_LIN   EQU 26
+SPAWN1_SND_COL  EQU 26
+SPAWN2_SND_COL  EQU 32
+SPAWN3_SND_COL  EQU 48
+
+LIN_PAINEL  EQU 25
+COL_PAINEL  EQU 27
+
 ; * Tamanhos
 LARGURA     EQU 5
-COMPRIMENTO EQU 5
+ALTURA EQU 5
 LAR_PAINEL  EQU 15
-COM_PAINEL  EQU 5
+ALT_PAINEL  EQU 5
 LAR_SONDA   EQU 1
-COM_SONDA   EQU 1
+ALT_SONDA   EQU 1
 
 ; * Cores
 VERMELHO    EQU 0FF00H
@@ -72,7 +79,7 @@ APAGADO     EQU 0000H
 
 ; * Definições
 DEF_MET_MIN:
-    WORD COMPRIMENTO, LARGURA
+    WORD ALTURA, LARGURA
     WORD     0, VERDE, VERDE, VERDE, 0
     WORD VERDE, VERDE, VERDE, VERDE, VERDE
     WORD VERDE, VERDE, VERDE, VERDE, VERDE
@@ -80,15 +87,23 @@ DEF_MET_MIN:
     WORD     0, VERDE, VERDE, VERDE, 0
 
 DEF_MET_NMIN:
-    WORD COMPRIMENTO, LARGURA
+    WORD ALTURA, LARGURA
     WORD VERMELHO, 0, VERMELHO, 0, VERMELHO
     WORD 0, VERMELHO, VERMELHO, VERMELHO, 0
     WORD VERMELHO, VERMELHO, 0, VERMELHO, VERMELHO
     WORD 0, VERMELHO, VERMELHO, VERMELHO, 0
     WORD VERMELHO, 0, VERMELHO, 0, VERMELHO
 
+DEF_EXPLOSAO:
+    WORD ALTURA, LARGURA
+    WORD 0, AZUL, 0, AZUL, 0
+    WORD AZUL, 0, AZUL, 0, AZUL
+    WORD 0, AZUL, 0, AZUL, 0
+    WORD AZUL, 0, AZUL, 0, AZUL
+    WORD 0, AZUL, 0, AZUL, 0
+
 DEF_PAINEL:
-    WORD COM_PAINEL, LAR_PAINEL
+    WORD ALT_PAINEL, LAR_PAINEL
     WORD 0, 0, VERMELHO, VERMELHO, VERMELHO, VERMELHO, VERMELHO, VERMELHO, VERMELHO, VERMELHO, VERMELHO, VERMELHO, VERMELHO, 0, 0
     WORD 0, VERMELHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, VERMELHO, 0
     WORD VERMELHO, CASTANHO, CASTANHO, CASTANHO, 0, 0, 0, 0, 0, 0, 0, CASTANHO, CASTANHO, CASTANHO, VERMELHO
@@ -96,7 +111,7 @@ DEF_PAINEL:
     WORD VERMELHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, CASTANHO, VERMELHO
 
 DEF_SONDA:
-    WORD COM_SONDA, LAR_SONDA
+    WORD ALT_SONDA, LAR_SONDA
     WORD ROSA
 
 
@@ -110,6 +125,8 @@ pilha:
     STACK 100H
 
 SP_Inicial:
+
+
 ; **********************************************************************
 ; * Código
 ; **********************************************************************
@@ -119,20 +136,34 @@ PLACE 0
 ; corpo principal do programa
 
 inicializacoes:
-    MOV  SP, SP_Inicial; Inicialização do Stack Pointer
-    MOV  R2, TEC_LIN   ; endereço do periférico das linhas
-    MOV  R3, TEC_COL   ; endereço do periférico das colunas
-    MOV  R4, DISPLAYS  ; endereço do periférico dos displays
-    MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso
-    MOV  R8, 0H        ; contador de clicks
+    ; * Stack Pointer
+    MOV SP, SP_Inicial      ; Inicialização do Stack Pointer
 
-; corpo principal do programa
-ciclo:                   ; inicia o ciclo
-    MOV  R9, 0H          ; auxiliar para apresentar no display
-    MOV  R7, 0H          ; auxiliar para calcular a tecla
-    MOVB [R4], R7        ; escreve linha e coluna a zero nos displays
+    ; * Ecrâ
+    MOV [APAGA_AVISO], R0	            ; apaga o aviso do ecrã (R0 não é relevante)
+    MOV [APAGA_ECRÃ], R0	            ; apaga todos os pixels já desenhados (R0 não é relevante)
+    MOV R0, 0                           ; cenário de fundo número 0
+    MOV [SELECIONA_CENARIO_FUNDO], R0   ; seleciona o cenário de fundo
 
-    MOV  R1, U_LINHA     ; volta à última linha
+    ; * Gerais
+    MOV R5, MASCARA                     ; para isolar os 4 bits de menor peso
+    MOV R7, 0H                          ; contador de clicks no teclado
+
+; desenhar meteoro minerável
+posicao_boneco:
+    MOV R1, SPAWN_LIN   ; linha do meteoro
+    MOV R2, SPAWN1_COL  ; linha do meteoro
+    MOV R4, DEF_MET_MIN ; endereço da tabela do meteoro minerável
+
+mostra_boneco:
+    CALL desenha_boneco ; desenha o boneco a partir da tabela
+
+ciclo_teclado:              ; inicia o ciclo
+    MOV  R4, 0H             ; auxiliar para apresentar no display
+    MOV  R3, 0H             ; auxiliar para calcular a tecla
+    MOVB [DISPLAYS], R3     ; escreve linha e coluna a zero nos displays
+
+    MOV  R1, U_LINHA        ; volta à última linha
     JMP espera_tecla
 
 passa_linha:
@@ -140,36 +171,92 @@ passa_linha:
     JZ ciclo           ; se for 0, reinicia o ciclo
 
 espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
-    MOVB [R2], R1      ; escrever no periférico de saída (linhas)
-    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
+    MOVB [TEC_LIN], R1      ; escrever no periférico de saída (linhas)
+    MOVB R0, [TEC_COL]      ; ler do periférico de entrada (colunas)
     AND  R0, R5        ; elimina bits para além dos bits 0-3
     CMP  R0, 0         ; há tecla premida?
     JZ   passa_linha   ; se nenhuma tecla premida, repete
                        ; vai mostrar a linha e a coluna da tecla
 
-    ADD R8, 1
+    ADD R7, 1
     MOV R6, R1         ; guarda a linha atual, e R1 passa a auxiliar
 
     CALL converte      ; converte a linha
     MOV R1, 4
-    MUL R7, R1         ; multiplica a linha por 4
+    MUL R3, R1         ; multiplica a linha por 4
     MOV R1, R0         ; passa a coluna para o registo R1
     CALL converte      ; converte a coluna
-    OR R9, R8
-    SHL R9, 4
-    OR R9, R7
-    MOVB [R4], R9      ; escreve linha e coluna nos displays
+    OR R4, R7
+    SHL R4, 4
+    OR R4, R3
+    MOVB [DISPLAYS], R4      ; escreve linha e coluna nos displays
 
 ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
-    MOVB [R2], R6      ; escrever no periférico de saída (linhas)
-    MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
+    MOVB [TEC_LIN], R6      ; escrever no periférico de saída (linhas)
+    MOVB R0, [TEC_COL]      ; ler do periférico de entrada (colunas)
     AND  R0, R5        ; elimina bits para além dos bits 0-3
     CMP  R0, 0         ; há tecla premida?
     JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera até não haver
-    JMP  ciclo         ; repete ciclo
+    JMP  ciclo_teclado ; repete ciclo
 
 
-; R1 - numero a transformar, R2 - contador, return R7 somado
+; **********************************************************************
+; DESENHA_BONECO - Desenha um boneco na linha e coluna indicadas
+;			    com a forma e cor definidas na tabela indicada.
+; Argumentos:   R1 - linha
+;               R2 - coluna
+;               R4 - tabela que define o boneco
+;
+; **********************************************************************
+desenha_boneco:
+	PUSH R2
+	PUSH R3
+	PUSH R4
+	PUSH R5
+    PUSH R6
+	MOV	 R5, [R4]		; obtém a altura do boneco
+    MOV  R6, [R4+2]     ; obtém a largura do boneco
+	ADD	 R4, 4			; endereço da cor do 1º pixel
+desenha_pixels:       	; desenha os pixels do boneco a partir da tabela
+	MOV	 R3, [R4]		; obtém a cor do próximo pixel do boneco
+	CALL escreve_pixel  ; escreve cada pixel do boneco
+	ADD	 R4, 2			; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
+    ADD  R2, 1          ; próxima coluna
+    SUB  R6, 1			; menos uma coluna para tratar
+    JNZ  desenha_pixels ; continua até percorrer toda a largura do objeto
+    ADD	 R4, 2			; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
+    ADD  R1, 1          ; próxima linha
+    SUB  R5, 1			; menos uma linha para tratar
+    JNZ  desenha_pixels ; continua até percorrer toda a largura do objeto
+    POP  R6
+	POP	 R5
+	POP	 R4
+	POP	 R3
+	POP	 R2
+	RET
+
+
+; **********************************************************************
+; ESCREVE_PIXEL - Escreve um pixel na linha e coluna indicadas.
+; Argumentos:   R1 - linha
+;               R2 - coluna
+;               R3 - cor do pixel (em formato ARGB de 16 bits)
+;
+; **********************************************************************
+escreve_pixel:
+	MOV  [DEFINE_LINHA], R1		; seleciona a linha
+	MOV  [DEFINE_COLUNA], R2		; seleciona a coluna
+	MOV  [DEFINE_PIXEL], R3		; altera a cor do pixel na linha e coluna já selecionadas
+	RET
+
+
+; **********************************************************************
+; CONVERTE - Converte a linha, ou coluna, para um número entre 0 e 3.
+;
+; Argumentos:   R1 - linha/coluna
+;
+; Retorna:      R3 - Soma das conversões
+; **********************************************************************
 converte:
     PUSH R2
     PUSH R1
@@ -179,7 +266,8 @@ converte_loop:
     SHR R1, 1
     JNZ converte_loop
     SUB R2, 1           ; retira 1 para passar a um numero entre 0 e 3
-    ADD R7, R2
+    ADD R3, R2
     POP R1
     POP R2
     RET
+
